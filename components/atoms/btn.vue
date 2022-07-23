9831b9e91541a0255e1e9e616587f439054aa513
Taishi-Ito <taishi.ito1026@gmail.com>
2022-07-23 11:54:40 +0900

    Setup for axios communication

---
+++ components/atoms/btn.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-btn depressed color="primary" @click="$store.dispatch('fireauth/updateMessageAction', 'update!!!')">{{ btnTitle }}</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      btnTitle: "axios通信確認"
+    }
+  }
+}
+</script>
