64d37a7c12e05609a09edc4e71e75e683aff8869
Taishi-Ito <taishi.ito1026@gmail.com>
2022-08-11 11:27:52 +0900

    Refactor all methods to send idToken correctly

--- store/auth.js
+++ store/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, deleteUser, onAuthStateChanged} from 'firebase/auth'
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, deleteUser, onAuthStateChanged, reauthenticateWithCredential, EmailAuthProvider} from 'firebase/auth'
 
 export const state = () => ({
   message: 'Hello Vuex!',
@@ -7,7 +7,8 @@ export const state = () => ({
   userId: '',
   email: '',
   name: '',
-  locale: ''
+  locale: '',
+  idToken: ''
 })
 
 export const getters = ({
@@ -39,11 +40,14 @@ export const mutations = {
     state.locale = locale
   },
   clearUserStatus(state) {
-    state.isLoggedIn = ""
+    state.isLoggedIn = false
     state.userId = ""
     state.email = ""
     state.name = ""
     state.locale = ""
+  },
+  setIdToken(state, idToken) {
+    state.idToken = idToken
   }
 }
 
@@ -51,7 +55,7 @@ export const actions = {
   updateMessageAction(context, payload) {
     const url = `${process.env.url}/api/v1/hello`
     let query = payload
-    axios.get(url, {params:{message: query}})
+    axios.get(url, {params:{token: context.state.idToken, message: query}})
     .then((res) =>{
       context.commit('updateMessage', res.data)
     })
@@ -60,10 +64,36 @@ export const actions = {
       console.log('【updateMessageAction error】', e)
     })
   },
+  async getIdToken(context) {
+    const auth = getAuth();
+    await auth.currentUser.getIdToken(/* forceRefresh */ true)
+    .then(function(idToken) {
+      context.commit('setIdToken', idToken)
+    })
+    .catch((e) => {
+      console.log('getIdToken error', e)
+    });
+  },
+  async getUserInfo(context, uid) {
+    const url = `${process.env.url}/api/v1/users/${uid}`;
+    await axios.get(url, { headers: {Authorization: `Bearer ${context.state.idToken}`}, params: {token: context.state.idToken, "uid": uid}})
+    .then((res) =>{
+      if (res.data["is_name"]) {
+        context.commit('setName', res.data["name"])
+        context.commit('setLocale', res.data["locale"])
+      }
+    })
+    .catch( e => {
+      alert("ユーザー情報取得中にエラーが発生しました")
+      console.log('signin getuser error】', e)
+    })
+  },
   signUp(context, payload) {
     const auth = getAuth(this.$firebase)
     createUserWithEmailAndPassword(auth, payload["email"], payload["password"])
     .then( userCredential => {
+      context.commit('setUserId', userCredential.user.uid)
+      context.commit('setEmail', userCredential.user.email)
       this.$router.push('/auth/registerBackUserInfo')
     })
     .catch( e => {
@@ -72,74 +102,52 @@ export const actions = {
     })
   },
   async registerBackUserInfo(context, payload) {
-    const that = this
-    const auth = getAuth();
-    await auth.currentUser.getIdToken(/* forceRefresh */ true)
-    .then(function(idToken) {
-      const email = auth.currentUser.email;
-      const url = `${process.env.url}/api/v1/users`;
-      axios.post(url, {token: idToken, user: {"name": payload["name"], "locale": "ja"}})
-      .then((res) =>{
-        if (res.data.name) {
-          const payload = {"uid": res.data["uid"], "email": email, "name": res.data["name"], "locale": res.data["locale"]}
-          context.dispatch('addUserInfo', payload)
-          that.$router.push('/dashboard')
-        } else {
-          alert("名前を登録できませんでした")
-          console.log('registerBackUserInfo validation error】')
-        }
-      })
-      .catch((e) => {
+    await context.dispatch('getIdToken')
+    const url = `${process.env.url}/api/v1/users`;
+    axios.post(url, {token: context.state.idToken, user: {"name": payload["name"], "locale": "ja"}})
+    .then((res) =>{
+      if (res.data.name) {
+        context.commit('setName', res.data["name"])
+        context.commit('setLocale', res.data["locale"])
+        context.commit('setSigninStatus', true)
+        this.$router.push('/dashboard')
+      } else {
         alert("名前を登録できませんでした")
-        console.log('registerBackUserInfo error】', e)
-      });
+        console.log('no res.data.name')
+      }
     })
     .catch((e) => {
-      console.log('トークン取得エラー', e)
+      alert("名前を登録できませんでした")
+      console.log('registerBackUserInfo error', e)
     });
   },
   async signin(context, payload) {
     const auth = getAuth(this.$firebase)
+    let uid = ""
     await signInWithEmailAndPassword(auth, payload["email"], payload["password"])
     .then( userCredential => {
-      const uid = userCredential.user.uid;
-      const email = userCredential.user.email;
-      const url = `${process.env.url}/api/v1/users/${uid}`;
-      axios.get(url, {params: {token: userCredential.user.accessToken, "uid": uid}})
-      .then((res) =>{
-        if (res.data["is_user"]) {
-          if (res.data["is_name"]) {
-            const payload = {"uid": uid, "email": email, "name": res.data["name"], "locale": res.data["locale"]}
-            context.dispatch('addUserInfo', payload)
-            this.$router.push('/dashboard')
-          } else {
-            alert("名前を登録してください")
-            this.$router.push('/auth/registerBackUserInfo')
-          }
-        } else {
-          alert("名前を登録してください")
-          this.$router.push('/auth/registerBackUserInfo')
-        }
-      })
-      .catch( e => {
-        alert("ユーザー情報取得中にエラーが発生しました")
-        console.log('signin getuser error】', e)
-      })
+      uid = userCredential.user.uid;
+      context.commit('setUserId', uid)
+      context.commit('setEmail', userCredential.user.email)
     })
     .catch( e => {
-      alert("ログイン情報を取得できません")
+      alert("ログインできません")
       console.log('【signin error】', e)
     })
+    await context.dispatch('getIdToken')
+    await context.dispatch('getUserInfo', uid)
+    if (context.state.name) {
+      context.commit('setSigninStatus', true)
+      this.$router.push('/dashboard')
+    } else {
+      this.$router.push('/auth/registerBackUserInfo')
+    }
   },
   async signOut(context) {
     const auth = getAuth(this.$firebase)
     await signOut(auth)
     .then(()=>{
-      context.commit('setSigninStatus', false)
-      context.commit('setUserId', '')
-      context.commit('setEmail', '')
-      context.commit('setName', '')
-      context.commit('setLocale', '')
+      context.commit('clearUserStatus')
       this.$router.push('/auth/signin')
     })
     .catch( e => {
@@ -147,53 +155,50 @@ export const actions = {
       console.log('【signOut error】', e)
     })
   },
-  addUserInfo(context, payload) {
-    context.commit('setSigninStatus', true)
-    context.commit('setUserId', payload["uid"])
-    context.commit('setEmail', payload["email"])
-    context.commit('setName', payload["name"])
-    context.commit('setLocale', payload["locale"])
-  },
   async deleteUser(context) {
+    await context.dispatch('deleteFirebaseUserInfo', "password")
+    .then(() => {
+      context.dispatch('deleteBackUserInfo')
+    })
+    .then(() => {
+      context.commit('clearUserStatus')
+      this.$router.push('/auth/signin')
+    })
+  },
+  async deleteFirebaseUserInfo(context, password) {
     const auth = getAuth();
     const user = auth.currentUser;
-    const uid = auth.currentUser.uid;
-    try {
-      context.dispatch('deleteFirebaseUserInfo', user)
-    } catch(e) {
-      console.log('addUserInfo deleteFirebaseUserInfo】', e)
-      return
-    }
-    try {
-      context.dispatch('deleteBackUserInfo', uid)
-    } catch(e) {
-      console.log('addUserInfo deleteBackUserInfo', e)
-      return
-    }
-    this.$router.push('/auth/register')
-  },
-  async deleteFirebaseUserInfo(context, user) {
-    await deleteUser(user)
+    const credential = EmailAuthProvider.credential(user.email, password)
+    let is_credentialed = false
+    await reauthenticateWithCredential(user, credential)
     .then(() => {
-      context.commit('setSigninStatus', false)
-      context.commit('setUserId', '')
-      context.commit('setEmail', '')
+      is_credentialed = true
     })
     .catch((e) => {
-      alert("ユーザー情報を削除できませんでした")
-      console.log('deleteFirebaseUserInfo error】', e)
+      console.log('cant get credential', e)
     });
+    if (is_credentialed) {
+      await deleteUser(user)
+      .then(() => {
+        return
+      })
+      .catch((e) => {
+        alert("ユーザー情報を削除できませんでした")
+        console.log('deleteFirebaseUserInfo error', e)
+      });
+    } else {
+      return
+    }
   },
-  deleteBackUserInfo(context, uid) {
+  deleteBackUserInfo(context) {
     const url = `${process.env.url}/api/v1/users/destroy`;
-    axios.delete(url, {params: {"uid": uid}})
+    axios.delete(url, {params: {token: context.state.idToken, "uid": context.state.userId}})
     .then((res) =>{
-      context.commit('setName', '')
-      context.commit('setLocale', '')
+      return
     })
     .catch((e) => {
-      alert("ユーザー情報を削除できませんでした")
-      console.log('deleteBackUserInfo error】', e)
+      console.log('【backend destroy error】', e)
+      this.$router.push('/auth/signin')
     });
   }
 }
