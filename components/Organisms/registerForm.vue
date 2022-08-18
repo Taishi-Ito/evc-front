fa9ae77261ec4dbd05e92aef1216946f0f7145cd
Taishi-Ito <taishi.ito1026@gmail.com>
2022-08-18 22:48:22 +0900

    Style signin and register form

--- components/Organisms/registerForm.vue
+++ components/Organisms/registerForm.vue
@@ -1,42 +1,49 @@
 <template>
-  <validation-observer ref="observer" v-slot="{ invalid }">
-    <form @submit.prevent="submit">
-      <MoleculesInputForm
-        :formContent="email"
-        :formLabel="'メールアドレス'"
-        :formId="'signUpEmail'"
-        :formName="'メールアドレス'"
-        :formRules="'required|email'"
-        :formType="'text'"
-        @update="email=$event"
-      ></MoleculesInputForm>
-      <MoleculesInputForm
-        :formContent="password"
-        :formLabel="'パスワード'"
-        :formId="'signUpPassword'"
-        :formName="'パスワード'"
-        :formRules="'required|min:6'"
-        :formType="'password'"
-        @update="password=$event"
-      ></MoleculesInputForm>
-      <MoleculesInputForm
-        :formContent="passwordConfirmation"
-        :formLabel="'新しいパスワード(確認)'"
-        :formId="'signUpPasswordConfirmation'"
-        :formName="'新しいパスワード(確認)'"
-        :formRules="'required|confirmed:パスワード'"
-        :formType="'password'"
-        @update="passwordConfirmation=$event"
-      ></MoleculesInputForm>
-      <AtomsSendBtn
-        :btnId="'register'"
-        :btnTitle="'送信'"
-        :btnType="'submit'"
-        :btnDisabled="invalid"
-        :btnColor="'primary'"
-      ></AtomsSendBtn>
-    </form>
-  </validation-observer>
+  <div class="wholeContainer">
+    <v-card class="card" elevation="2">
+      <h2 class="cardTitle">
+        ユーザー登録
+      </h2>
+      <validation-observer ref="observer" v-slot="{ invalid }">
+        <form @submit.prevent="submit">
+          <MoleculesInputForm
+            :formContent="email"
+            :formLabel="'メールアドレス'"
+            :formId="'signUpEmail'"
+            :formName="'メールアドレス'"
+            :formRules="'required|email'"
+            :formType="'text'"
+            @update="email=$event"
+          ></MoleculesInputForm>
+          <MoleculesInputForm
+            :formContent="password"
+            :formLabel="'パスワード'"
+            :formId="'signUpPassword'"
+            :formName="'パスワード'"
+            :formRules="'required|min:6'"
+            :formType="'password'"
+            @update="password=$event"
+          ></MoleculesInputForm>
+          <MoleculesInputForm
+            :formContent="passwordConfirmation"
+            :formLabel="'新しいパスワード(確認)'"
+            :formId="'signUpPasswordConfirmation'"
+            :formName="'新しいパスワード(確認)'"
+            :formRules="'required|confirmed:パスワード'"
+            :formType="'password'"
+            @update="passwordConfirmation=$event"
+          ></MoleculesInputForm>
+          <AtomsSendBtn
+            :btnId="'register'"
+            :btnTitle="'送信'"
+            :btnType="'submit'"
+            :btnDisabled="invalid"
+            :btnColor="'primary'"
+          ></AtomsSendBtn>
+        </form>
+      </validation-observer>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -61,3 +68,20 @@
   }
   }
 </script>
+<style>
+.wholeContainer {
+  width: 80%;
+  max-width: 600px;
+  margin: 0 auto;
+}
+.card {
+  padding-top: 30px;
+  padding-bottom: 30px;
+  width: 100%;
+  margin-top: 30px;
+  text-align: center;
+}
+.cardTitle {
+  margin-bottom: 20px;
+}
+</style>
--- components/Organisms/signinForm.vue
+++ components/Organisms/signinForm.vue
@@ -1,33 +1,40 @@
 <template>
-  <validation-observer ref="observer" v-slot="{ invalid }">
-    <form @submit.prevent="submit">
-      <MoleculesInputForm
-        :formContent="email"
-        :formLabel="'メールアドレス'"
-        :formId="'emailField'"
-        :formName="'emailField'"
-        :formRules="'required|email'"
-        :formType="'text'"
-        @update="email=$event"
-      ></MoleculesInputForm>
-      <MoleculesInputForm
-        :formContent="password"
-        :formLabel="'パスワード'"
-        :formId="'passwordField'"
-        :formName="'passwordField'"
-        :formRules="'required|min:6'"
-        :formType="'password'"
-        @update="password=$event"
-      ></MoleculesInputForm>
-      <AtomsSendBtn
-        :btnId="'signin'"
-        :btnTitle="'送信'"
-        :btnType="'submit'"
-        :btnDisabled="invalid"
-        :btnColor="'primary'"
-      ></AtomsSendBtn>
-    </form>
-  </validation-observer>
+  <div class="wholeContainer">
+    <v-card class="card" elevation="2">
+      <h2 class="cardTitle">
+        ログイン
+      </h2>
+      <validation-observer ref="observer" v-slot="{ invalid }">
+        <form @submit.prevent="submit">
+          <MoleculesInputForm
+            :formContent="email"
+            :formLabel="'メールアドレス'"
+            :formId="'emailField'"
+            :formName="'emailField'"
+            :formRules="'required|email'"
+            :formType="'text'"
+            @update="email=$event"
+          ></MoleculesInputForm>
+          <MoleculesInputForm
+            :formContent="password"
+            :formLabel="'パスワード'"
+            :formId="'passwordField'"
+            :formName="'passwordField'"
+            :formRules="'required|min:6'"
+            :formType="'password'"
+            @update="password=$event"
+          ></MoleculesInputForm>
+          <AtomsSendBtn
+            :btnId="'signin'"
+            :btnTitle="'送信'"
+            :btnType="'submit'"
+            :btnDisabled="invalid"
+            :btnColor="'primary'"
+          ></AtomsSendBtn>
+        </form>
+      </validation-observer>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -52,3 +59,20 @@ export default {
   }
 }
 </script>
+<style>
+.wholeContainer {
+  width: 80%;
+  max-width: 600px;
+  margin: 0 auto;
+}
+.card {
+  padding-top: 30px;
+  padding-bottom: 30px;
+  width: 100%;
+  margin-top: 30px;
+  text-align: center;
+}
+.cardTitle {
+  margin-bottom: 20px;
+}
+</style>
