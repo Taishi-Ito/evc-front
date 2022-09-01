cf06d2ec94db7ebf7e549d381596f8782a9b50a9
Taishi-Ito <taishi.ito1026@gmail.com>
2022-09-01 18:22:54 +0900

    Create work group and projects lists

--- components/Organisms/projects.vue
+++ components/Organisms/projects.vue
@@ -1,35 +1,54 @@
 <template>
-  <v-list-group :value="false" no-action sub-group>
-    <template v-slot:activator>
-      <v-list-item-title v-text="projectsData['workGroupTitle']['title']"></v-list-item-title>
-      <v-list-item-icon class="buttons">
-        <v-btn icon dark>
-          <v-icon v-text="'mdi-pencil'" small :color="buttonColor"></v-icon>
-        </v-btn>
-        <v-btn icon dark>
-          <v-icon v-text="'mdi-trash-can'" small :color="buttonColor"></v-icon>
-        </v-btn>
-      </v-list-item-icon>
-    </template>
+  <div>
+    <v-dialog v-model="showDialog" width="500">
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">{{ title }}</v-card-title>
+        <div class="textFieldContainer">
+          <div class="inner">
+            <v-text-field v-model="content" :label="label"/>
+          </div>
+        </div>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="execTextFieldDialog()">{{ btnText }}</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-    <v-list-item :key="'new' + i" link>
-      <v-list-item-title v-text="'新規作成'"></v-list-item-title>
-      <v-list-item-icon>
-        <v-icon v-text="'mdi-plus-thick'" small :color="buttonColor"></v-icon>
-      </v-list-item-icon>
-    </v-list-item>
-    <v-list-item v-for="(projectTitle) in projectsData['projectTitles']" :key="projectTitle['id']" link>
-      <v-list-item-title v-text="projectTitle['title']"></v-list-item-title>
+    <v-list-group :value="false" no-action sub-group>
+      <template v-slot:activator>
+        <v-list-item-title v-text="projectsData['workGroupTitle']['title']"></v-list-item-title>
         <v-list-item-icon class="buttons">
-          <v-btn icon dark>
+          <v-btn icon dark @click.stop="openTextFieldDialog('edit', 'workGroup', {'data': projectsData['workGroupTitle'], 'title': 'ワークグループ名変更', 'label': 'ワークグループ名', 'btnText': '変更'})">
             <v-icon v-text="'mdi-pencil'" small :color="buttonColor"></v-icon>
           </v-btn>
           <v-btn icon dark>
             <v-icon v-text="'mdi-trash-can'" small :color="buttonColor"></v-icon>
           </v-btn>
         </v-list-item-icon>
-    </v-list-item>
-  </v-list-group>
+      </template>
+
+      <v-list-item :key="'new' + i" link @click.stop="openTextFieldDialog('new', 'project',{'data': projectsData['workGroupTitle'], 'title': 'プロジェクト新規作成', 'label': 'プロジェクト名', 'btnText': '新規作成'})">
+        <v-list-item-title v-text="'新規作成'"></v-list-item-title>
+        <v-list-item-icon>
+          <v-icon v-text="'mdi-plus-thick'" small :color="buttonColor"></v-icon>
+        </v-list-item-icon>
+      </v-list-item>
+      <v-list-item v-for="(projectTitle) in projectsData['projectTitles']" :key="projectTitle['id']" link>
+        <v-list-item-title v-text="projectTitle['title']"></v-list-item-title>
+          <v-list-item-icon class="buttons">
+            <v-btn icon dark @click.stop="openTextFieldDialog('edit', 'project', {'data': projectTitle, 'title': 'プロジェクト名変更', 'label': 'プロジェクト名', 'btnText': '変更'})">
+              <v-icon v-text="'mdi-pencil'" small :color="buttonColor"></v-icon>
+            </v-btn>
+            <v-btn icon dark>
+              <v-icon v-text="'mdi-trash-can'" small :color="buttonColor"></v-icon>
+            </v-btn>
+          </v-list-item-icon>
+      </v-list-item>
+    </v-list-group>
+  </div>
+
 </template>
 
 <script>
@@ -48,8 +67,45 @@ export default {
   data () {
     return {
       buttonColor: "blue-grey lighten-3",
+      showDialog: false,
+      title: "",
+      label: "",
+      content: "",
+      btnText: "",
+      id: null,
+      type: "",
+      target: ""
     }
   },
+  methods: {
+    openTextFieldDialog(type, target, payload) {
+      this.title = payload["title"]
+      this.label = payload["label"]
+      this.id = payload["data"]["id"]
+      this.btnText = payload["btnText"]
+      this.showDialog = true
+      this.type = type
+      this.target = target
+      if (type == "edit") {
+        this.content = payload["data"]["title"]
+      } else {
+        this.content = ""
+      }
+    },
+    execTextFieldDialog() {
+      if (this.type == "edit" && this.target == "workGroup") {
+        const payload = {"title": this.content, "id": this.id}
+        this.$store.dispatch('dashboard/changeWorkGroupTitle', payload)
+      } else if (this.type == "edit" && this.target == "project") {
+        const payload = {"title": this.content, "id": this.id}
+        this.$store.dispatch('dashboard/changeProjectTitle', payload)
+      } else if (this.type == "new" && this.target == "project") {
+        const payload = {"title": this.content, "work_group_id": this.id}
+        this.$store.dispatch('dashboard/createProject', payload)
+      }
+      this.showDialog = false
+    }
+  }
 
 }
 </script>
@@ -57,5 +113,9 @@ export default {
 <style scoped>
 .buttons {
   margin-right: 20px;
+}
+.inner {
+  width: 80%;
+  margin: 0 auto;
 }
 </style>
--- components/Organisms/workGroups.vue
+++ components/Organisms/workGroups.vue
@@ -1,31 +1,84 @@
 <template>
-  <v-list-group :value="false" prepend-icon="mdi-apps" dark>
-    <template v-slot:activator>
-      <v-list-item-title>ワークグループ</v-list-item-title>
-    </template>
+  <div>
+    <v-list-group :value="false" prepend-icon="mdi-apps" dark>
+      <template v-slot:activator>
+        <v-list-item-title>ワークグループ</v-list-item-title>
+      </template>
 
-    <div v-for="(projectsData, i) in projectsDatas">
-      <OrganismsProjects
-        :projectsData="projectsData"
-        :i="i"
-      ></OrganismsProjects>
-    </div>
-  </v-list-group>
+      <v-list-item :key="'neWorkGroup'" @click="openTextFieldDialog('new', 'workGroup', {'title': 'ワークグループ新規作成', 'label': 'ワークグループ名', 'btnText': '新規作成'})">
+        <v-list-item-title v-text="'新規作成'" class="neWorkGroup"></v-list-item-title>
+        <v-list-item-icon>
+          <v-icon v-text="'mdi-plus-thick'" small :color="buttonColor"></v-icon>
+        </v-list-item-icon>
+      </v-list-item>
+
+      <div v-for="(projectsData, i) in workGroupProjectLists">
+        <OrganismsProjects
+          :projectsData="projectsData"
+          :i="i"
+        ></OrganismsProjects>
+      </div>
+    </v-list-group>
+
+    <v-dialog v-model="showDialog" width="500">
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">{{ title }}</v-card-title>
+        <div class="textFieldContainer">
+          <div class="inner">
+            <v-text-field v-model="content" :label="label"/>
+          </div>
+        </div>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="execTextFieldDialog()">{{ btnText }}</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      projectsDatas: [
-        {"workGroupTitle": {"title": "化学メーカー", "id": 1}, "projectTitles": [{"title": "積水化学", "id": 1},{"title": "日東電工", "id": 2}]},
-        {"workGroupTitle": {"title": "自動車メーカー", "id": 2}, "projectTitles": [{"title": "トヨタ自動車", "id": 3},{"title": "日産自動車", "id": 4}]}
-        ]
+      buttonColor: "blue-grey lighten-3",
+      showDialog: false,
+      title: "",
+      label: "",
+      content: "",
+      btnText: ""
     }
   },
+  computed: {
+    workGroupProjectLists(){
+      return this.$store.getters["dashboard/workGroupProjectLists"]
+    }
+  },
+  methods: {
+    openTextFieldDialog(type, target, payload) {
+      this.title = payload["title"]
+      this.label = payload["label"]
+      this.btnText = payload["btnText"]
+      this.content = ""
+      this.showDialog = true
+    },
+    execTextFieldDialog() {
+      this.$store.dispatch('dashboard/createWorkGroup', this.content)
+      this.content = ""
+      this.showDialog = false
+    }
+  }
 
 }
 </script>
 
 <style scoped>
+.neWorkGroup {
+  margin-left: 56px;
+}
+.inner {
+  width: 80%;
+  margin: 0 auto;
+}
 </style>
